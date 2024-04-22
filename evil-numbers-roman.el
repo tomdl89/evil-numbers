d217de0c57436e7bec622221ffc576f01d03a2ac
Tom Dalziel <tom_dl@hotmail.com>
2024-04-22 23:05:49 +0200

    Basic roman numerals functions

---
+++ evil-numbers-roman.el
@@ -0,0 +1,126 @@
+;;; evil-numbers-roman.el --- Increment/decrement roman numerals -*- lexical-binding: t -*-
+
+;; Copyright (C) 2011 by Michael Markert
+;;               2020 by Julia Path
+;;               2024 by Tom Dalziel
+;; Author: Tom Dalziel <tom_dl@hotmail.com>
+;; Maintainer: Julia Path <julia@jpath.de>
+;; Contributors: Tom Dalziel <tom_dl@hotmail.com>
+;; URL: http://github.com/juliapath/evil-numbers
+;; Git-Repository: git://github.com/juliapath/evil-numbers.git
+;; Created: 2024-04-22
+;; Version: 0.7
+;; Package-Requires: ((emacs "24.1") (evil "1.2.0"))
+;; Keywords: convenience tools
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;; Known Bugs:
+;; See http://github.com/juliapath/evil-numbers/issues
+
+;;; Code:
+
+(require 'evil)
+
+(defvar evil-numbers/roman-subtractives
+  '(("IV" 4)
+    ("IX" 9)
+    ("XL" 40)
+    ("XC" 90)
+    ("CD" 400)
+    ("CM" 900)))
+
+(defvar evil-numbers/roman-additives
+  '(("I" 1)
+    ("V" 5)
+    ("X" 10)
+    ("L" 50)
+    ("C" 100)
+    ("D" 500)
+    ("M" 1000)))
+
+(defvar evil-numbers/bad-pairs
+  '((900 500) (900 400) (900 100)
+    (90 50) (90 40) (90 10)
+    (9 5) (9 4) (9 1)
+    (500 500) (500 400) (400 100)
+    (50 50) (50 40) (40 10)
+    (5 5) (5 4) (4 1)
+    (400 400) (40 40) (4 4)))
+
+(defun evil-numbers/tokenize-roman (s)
+  (let ((rx "\\(?1:IV\\|IX\\|XL\\|XC\\|CD\\|CM\\)\\|\\(?2:[IVXLCDM]\\)\\|\\(?3:.\\)")
+        (index 0)
+        case-fold-search
+        tokens)
+    (save-match-data
+      (while (string-match rx s index)
+        (push
+         (cond ((match-string 1 s) ; Subtractive tokens like IV and CM
+                (setq index (match-end 1))
+                (cadr (assoc (match-string 1 s) evil-numbers/roman-subtractives)))
+
+               ((match-string 2 s) ; Simple additive tokens like I and M
+                (setq index (match-end 2))
+                (cadr (assoc (match-string 2 s) evil-numbers/roman-additives)))
+
+               ((match-string 3 s) ; Not a valid token - return it as a string
+                (setq index (match-end 3))
+                (match-string 3 s)))
+         tokens)))
+    tokens))
+
+(defun evil-numbers/roman->int (s)
+  (let ((tokens (evil-numbers/tokenize-roman s))
+        (sum 0)
+        prev-token)
+    (while tokens
+      (cond ((stringp (car tokens))
+             (setq sum (format "Invalid token: %s" (car tokens))
+                   tokens nil))
+            ((or (member (list (car tokens) prev-token) evil-numbers/bad-pairs)
+                 (and prev-token (> prev-token (car tokens))))
+             (setq sum (format "Invalid token pair: %s %s" (car tokens) prev-token)
+                   tokens nil))
+            (t (setq sum (+ (car tokens) sum))))
+      (setq prev-token (car tokens)
+            tokens (cdr tokens)))
+    sum))
+
+(defun evil-numbers/digit->roman (digit place)
+  (if digit
+      (pcase place
+        (1 (nth digit '("" "I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX")))
+        (2 (nth digit '("" "X" "XX" "XXX" "XL" "L" "LX" "LXX" "LXXX" "XC")))
+        (3 (nth digit '("" "C" "CC" "CCC" "CD" "D" "DC" "DCC" "DCCC" "CM")))
+        (4 (make-string digit ?M))
+        (_ (format "%s digit numbers are out of range" place)))
+    "Non digit token"))
+
+(defun evil-numbers/int->roman (n)
+  (let* ((s-num (mapcar #'string (number-to-string n)))
+         (s-len (length s-num))
+         (roman-form ""))
+    (while (< 0 s-len)
+      (let ((roman-digit (evil-numbers/digit->roman (string-to-number (car s-num)) s-len)))
+        (setq roman-form (concat roman-form roman-digit)))
+      (setq s-num (cdr s-num)
+            s-len (1- s-len)))
+    roman-form))
